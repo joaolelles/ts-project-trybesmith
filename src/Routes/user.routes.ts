795db31429665fe8f64bfa939dc3b5439543b578
João <joaolelles7@gmail.com>
2023-03-15 17:02:47 -0300

    Requisito 7, Feat: Criando as validações dos usuárias

--- src/Routes/user.routes.ts
+++ src/Routes/user.routes.ts
@@ -1,8 +1,18 @@
 import { Router } from 'express';
 import userController from '../controller/user.controller';
+import validateUser from '../middleware/validateUser';
+
+const { validateUsername, validateVocation, validateLevel, validatePassword } = validateUser;
 
 const router = Router();
 
-router.post('/', userController.registerUser);
+router.post(
+  '/', 
+  validateUsername, 
+  validateVocation,
+  validateLevel, 
+  validatePassword, 
+  userController.registerUser,
+);
 
 export default router;
