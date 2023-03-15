048311d1b620893044609a81e722233cbc0b40be
João <joaolelles7@gmail.com>
2023-03-15 16:49:27 -0300

    Requisito 6, Feat: Criando as validações dos produtos

--- src/Routes/product.routes.ts
+++ src/Routes/product.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import productController from '../controller/product.controller';
+import validateProduct from '../middleware/validateProduct';
+
+const { validateName, validateAmount } = validateProduct;
 
 const router = Router();
 
-router.post('/', productController.registerProduct);
-router.get('/', productController.getAll);
+router.post('/', validateName, validateAmount, productController.registerProduct);
+router.get('/', validateName, validateAmount, productController.getAll);
 
 export default router;
